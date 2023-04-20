1f2313e3bfd663efeb222bbb50aac4aa010dcaab
Andy Andriamanga <andriamanga.andy@gmail.com>
2023-04-20 21:07:25 +0200

    Ajout des fonctions pour le fonctionnement d'Allegro

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <allegro.h>
 
-int main(void) {
-    printf("Hello World!\n");
+int main(void)
+{
+    allegro_init(); //initialisation
+    install_keyboard(); //clavier
+    install_mouse(); //souris
+    set_color_depth(desktop_color_depth());
+    if(set_gfx_mode(GFX_AUTODETECT_WINDOWED, 800, 600, 0, 0)!=0) //800x600 px
+    {
+        allegro_message("prb gfx mode");
+        allegro_exit();
+        exit(EXIT_FAILURE);
+    }
+    allegro_exit();
     return 0;
-}
+}END_OF_MAIN()
