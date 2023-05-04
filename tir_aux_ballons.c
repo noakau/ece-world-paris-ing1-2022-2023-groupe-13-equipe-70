d2febbe469ead48f5a159520024b5656c3f60a30
Noa <toto@toto.com>
2023-05-04 22:22:57 +0200

    Ajout des fichiers (vides) pour le jeu tirs aux ballons

---
+++ tir_aux_ballons.c
@@ -0,0 +1,5 @@
+//
+// Created by Noa on 04/05/2023.
+//
+
+#include "tir_aux_ballons.h"
---
+++ tir_aux_ballons.h
@@ -0,0 +1,8 @@
+//
+// Created by Noa on 04/05/2023.
+//
+
+#ifndef PROJET_TIR_AUX_BALLONS_H
+#define PROJET_TIR_AUX_BALLONS_H
+
+#endif //PROJET_TIR_AUX_BALLONS_H
